d119bfed74988d4bcad733e34b1534a32639f8c2
Jeff Lewis <jeffylew77@gmail.com>
2019-06-13 13:52:42 -0700

    [Dev] Importing Infinite Scroll screen

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
+// Imports: Dependencies
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+// Imports: Screens
+import InfiniteScroll from './screens/InfiniteScroll';
+
+// React Native: App
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* <Text>Open up App.js to start working on your app!</Text> */}
+      <InfiniteScroll />
     </View>
   );
 }
 
+// Styles
 const styles = StyleSheet.create({
   container: {
     flex: 1,
