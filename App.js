5a4cd47bb5abb57304177802d70fa73a5a6f5a28
Jeff Lewis <jeffylew77@gmail.com>
2019-06-13 14:05:13 -0700

    [Dev] Importing Firebase

--- App.js
+++ App.js
@@ -1,9 +1,25 @@
 // Imports: Dependencies
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as firebase from 'firebase';
+import 'firebase/firestore';
+import firebaseConfig from './config/config';
 
 // Imports: Screens
 import InfiniteScroll from './screens/InfiniteScroll';
+
+// Firebase: Initialize
+firebase.initializeApp({
+  apiKey: `${firebaseConfig.apiKey}`,
+  authDomain: `${firebaseConfig.authDomain}`,
+  databaseURL: `${firebaseConfig.databaseURL}`,
+  projectId: `${firebaseConfig.projectId}`,
+  storageBucket: `${firebaseConfig.storageBucket}`,
+  messagingSenderId: `${firebaseConfig.messagingSenderId}`,
+});
+
+// Firebase: Cloud Firestore
+export const database = firebase.firestore();
 
 // React Native: App
 export default function App() {
