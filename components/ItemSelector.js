c61cfdbc1ab022a2d0e5989910d618b411f35acd
Jeff Lewis <jeffylew77@gmail.com>
2019-06-13 14:44:15 -0700

    [Dev] Adding Item Selector

---
+++ components/ItemSelector.js
@@ -0,0 +1,51 @@
+// Imports: Dependencies
+import React, { Component } from "react";
+import { Dimensions, Image, View, SafeAreaView, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+// Screen Dimensions
+const { height, width } = Dimensions.get('window');
+
+// Item Selector
+export default class ItemSelector extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <TouchableOpacity onPress={this.props.onPress}>
+        <View style={styles.container}>
+          <Icon name="ios-arrow-forward" size={22} style={styles.arrowForward}/>
+          <Text style={styles.titleText}>{this.props.title}</Text>
+        </View>
+    </TouchableOpacity>
+    )
+  }
+}
+
+// Styles
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: width,
+    height: 70,
+    backgroundColor: '#fff',
+    borderColor: '#D4D4D2',
+    borderTopWidth: .2,
+  },
+  arrowForward: {
+    color: 'black',
+    opacity: .3,
+    position: 'absolute',
+    right: 20,
+  },
+  titleText: {
+    fontFamily: 'System',
+    fontSize: 18,
+    color: '#000',
+    fontWeight: '400',
+    marginLeft: 16,
+  },
+});
